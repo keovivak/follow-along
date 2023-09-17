b1a69c075d19a2d9b10376616c9d6f5c3d1b69ad
sylvi <keovivak@yahoo.com>
2023-09-17 10:02:57 -0500

    AGILE-000: DONE and DONE - Added comments and cleaned up some minor stuff - app def could use a lot of improvements and best practices - better functions and helpers - but the logic and flow works based on the acceptance criteria

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,6 @@
+/**
+ * Global consts
+ */
 const timelimit = 60;
 var quizTime = timelimit;
 var countdownTime = null
@@ -6,55 +9,67 @@ var currentQuestion = -1;
 var correctAnswer = 1;
 var leaderboard = []
 
+/**
+ * array of objects of quiz questions plus choices
+ * stored the actual html within the functions
+ * VERY HACKY and manual
+ * There is probably a more efficient way to generate HTML from javascript
+ * but I can't remember off the top of my head or bother to look at this time
+ * it works good enough but def could use a lot of improvements
+ * 
+ * LOTS OF ROOM for improvement
+ * using jquery and event listeners that will handle multiple functions
+ * using the create functions to generate the HTML
+ */
 const quizQuestions = [
     {
-        "question": "q1",
+        "question": "What kind of fish is Nemo from Finding Nemo?",
         "correctChoice": 0,
         "choices" : [
-            "<input type=\"radio\" id=\"choice1\" name=\"choices\" value=\"C1\"><label for=\"choice1\">C1</label><br>",
-            "<input type=\"radio\" id=\"choice2\" name=\"choices\" value=\"C2\"><label for=\"choice1\">C2</label><br>",
-            "<input type=\"radio\" id=\"choice3\" name=\"choices\" value=\"C3\"><label for=\"choice1\">C3</label><br>",
-            "<input type=\"radio\" id=\"choice4\" name=\"choices\" value=\"C4\"><label for=\"choice1\">C4</label><br>"
+            "<input type=\"radio\" id=\"choice1\" name=\"choices\" value=\"C1\"><label for=\"choice1\">Clownfish</label><br>",
+            "<input type=\"radio\" id=\"choice2\" name=\"choices\" value=\"C2\"><label for=\"choice1\">Shark</label><br>",
+            "<input type=\"radio\" id=\"choice3\" name=\"choices\" value=\"C3\"><label for=\"choice1\">Dory</label><br>",
+            "<input type=\"radio\" id=\"choice4\" name=\"choices\" value=\"C4\"><label for=\"choice1\">Goldfish</label><br>"
         ]
     },
     {
-        "question": "q2",
+        "question": "Who lives in a pineapple under the sea?",
         "correctChoice": 1,
         "choices" : [
-            "<input type=\"radio\" id=\"choice1\" name=\"choices\" value=\"C1\"><label for=\"choice1\">C11</label><br>",
-            "<input type=\"radio\" id=\"choice2\" name=\"choices\" value=\"C2\"><label for=\"choice1\">C2</label><br>",
-            "<input type=\"radio\" id=\"choice3\" name=\"choices\" value=\"C3\"><label for=\"choice1\">C3</label><br>",
-            "<input type=\"radio\" id=\"choice4\" name=\"choices\" value=\"C4\"><label for=\"choice1\">C4</label><br>"
+            "<input type=\"radio\" id=\"choice1\" name=\"choices\" value=\"C1\"><label for=\"choice1\">Mr. Crabbs</label><br>",
+            "<input type=\"radio\" id=\"choice2\" name=\"choices\" value=\"C2\"><label for=\"choice1\">Spongebob</label><br>",
+            "<input type=\"radio\" id=\"choice3\" name=\"choices\" value=\"C3\"><label for=\"choice1\">Squidward</label><br>",
+            "<input type=\"radio\" id=\"choice4\" name=\"choices\" value=\"C4\"><label for=\"choice1\">Patrick</label><br>"
         ]
     },
     {
-        "question": "q3",
+        "question": "What kind of animal is Simba",
         "correctChoice": 2,
         "choices" : [
-            "<input type=\"radio\" id=\"choice1\" name=\"choices\" value=\"C1\"><label for=\"choice1\">C111</label><br>",
-            "<input type=\"radio\" id=\"choice2\" name=\"choices\" value=\"C2\"><label for=\"choice1\">C2</label><br>",
-            "<input type=\"radio\" id=\"choice3\" name=\"choices\" value=\"C3\"><label for=\"choice1\">C3</label><br>",
-            "<input type=\"radio\" id=\"choice4\" name=\"choices\" value=\"C4\"><label for=\"choice1\">C4</label><br>"
+            "<input type=\"radio\" id=\"choice1\" name=\"choices\" value=\"C1\"><label for=\"choice1\">Baboon</label><br>",
+            "<input type=\"radio\" id=\"choice2\" name=\"choices\" value=\"C2\"><label for=\"choice1\">Warthog</label><br>",
+            "<input type=\"radio\" id=\"choice3\" name=\"choices\" value=\"C3\"><label for=\"choice1\">Lion</label><br>",
+            "<input type=\"radio\" id=\"choice4\" name=\"choices\" value=\"C4\"><label for=\"choice1\">Meerkat</label><br>"
         ]
     },
     {
-        "question": "q4",
+        "question": "What is Shrek?",
         "correctChoice": 3,
         "choices" : [
-            "<input type=\"radio\" id=\"choice1\" name=\"choices\" value=\"C1\"><label for=\"choice1\">C1111</label><br>",
-            "<input type=\"radio\" id=\"choice2\" name=\"choices\" value=\"C2\"><label for=\"choice1\">C2</label><br>",
-            "<input type=\"radio\" id=\"choice3\" name=\"choices\" value=\"C3\"><label for=\"choice1\">C3</label><br>",
-            "<input type=\"radio\" id=\"choice4\" name=\"choices\" value=\"C4\"><label for=\"choice1\">C4</label><br>"
+            "<input type=\"radio\" id=\"choice1\" name=\"choices\" value=\"C1\"><label for=\"choice1\">Goblin</label><br>",
+            "<input type=\"radio\" id=\"choice2\" name=\"choices\" value=\"C2\"><label for=\"choice1\">Troll</label><br>",
+            "<input type=\"radio\" id=\"choice3\" name=\"choices\" value=\"C3\"><label for=\"choice1\">Orc</label><br>",
+            "<input type=\"radio\" id=\"choice4\" name=\"choices\" value=\"C4\"><label for=\"choice1\">Ogre</label><br>"
         ]
     },
     {
-        "question": "q5",
+        "question": "How many sides does a square have?",
         "correctChoice": 0,
         "choices" : [
-            "<input type=\"radio\" id=\"choice1\" name=\"choices\" value=\"C1\"><label for=\"choice1\">C1111</label><br>",
-            "<input type=\"radio\" id=\"choice2\" name=\"choices\" value=\"C2\"><label for=\"choice1\">C2</label><br>",
-            "<input type=\"radio\" id=\"choice3\" name=\"choices\" value=\"C3\"><label for=\"choice1\">C3</label><br>",
-            "<input type=\"radio\" id=\"choice4\" name=\"choices\" value=\"C4\"><label for=\"choice1\">C4</label><br>"
+            "<input type=\"radio\" id=\"choice1\" name=\"choices\" value=\"C1\"><label for=\"choice1\">4</label><br>",
+            "<input type=\"radio\" id=\"choice2\" name=\"choices\" value=\"C2\"><label for=\"choice1\">6</label><br>",
+            "<input type=\"radio\" id=\"choice3\" name=\"choices\" value=\"C3\"><label for=\"choice1\">8</label><br>",
+            "<input type=\"radio\" id=\"choice4\" name=\"choices\" value=\"C4\"><label for=\"choice1\">5</label><br>"
         ]
     },
     {
@@ -77,7 +92,10 @@ const quizQuestions = [
     }
 ]
 
-
+/**
+ * timer function - taken from stackoverflow
+ * modified to not display negative time
+ */
 function myTimer() {
     if (quizTime > 0) {
         document.getElementById('timer').innerHTML = quizTime;
@@ -92,6 +110,13 @@ function myTimer() {
     }
 }
 
+/**
+ * Essentially the driver for the whole app
+ * On button click for start, it'll init the first question then process the next questions
+ * as long as not end of quiz or time is up will iterate through questions
+ * if starting quiz will start the timer
+ * end condition where if last question is answered or time is up will go to the submit name screen
+ */
 function startQuiz() {
     if (currentQuestion == 6) {
         restartQuiz();
@@ -124,6 +149,12 @@ function startQuiz() {
     currentQuestion += 1;
 }
 
+/**
+ * Initializes the quiz at the start
+ * Changes the HTML to represent the correct flow
+ * Gets the first question and sets
+ * Displays the choices for the first question
+ */
 function initializeQuiz() {
     document.getElementById("quizStart").innerHTML = "Next";
     document.getElementById("quizStart").id = "quizNext";
@@ -132,21 +163,45 @@ function initializeQuiz() {
     iterateChoices(quizQuestions[currentQuestion].choices);
 }
 
+/**
+ * This function gets the current question and sets it within the quizBox p tag on the HTML side
+ * using the question object within the questions array
+ */
 function getQuestion() {
     document.getElementById("quizBox").innerHTML = quizQuestions[currentQuestion].question;
 }
 
+/**
+ * questionChoices is the array object of the set of choices
+ * @param {*} questionChoices
+ * This function loops through the set of choices and then calls the setChoices() to display the values
+ */
 function iterateChoices(questionChoices) {
     for (let questionChoice=0; questionChoice<questionChoices.length; questionChoice++) {
         setChoices(questionChoice, questionChoices[questionChoice]);
     }
 }
 
+/**
+ * questionChoice represenets the value of the iterator from the loop who calls this function
+ * @param {*} questionChoice
+ * questionValue represents the actual value within the choices objects
+ * @param {*} questionValue 
+ * Called by a loop
+ * This function sets the choice value to the p tag
+ */
 function setChoices(questionChoice, questionValue) {
-    quizQuestionChoice = "quizQuestionChoice" + (questionChoice + 1);
+    let quizQuestionChoice = "quizQuestionChoice" + (questionChoice + 1);
     document.getElementById(quizQuestionChoice).innerHTML = questionValue;
 }
 
+/**
+ * Caputres the user selected answer
+ * iterates through all the buttons and determines which one the user selected
+ * saves the user choice then does a comparison to see if the user selected the right answer
+ * no need to iterate the choices because the object knows the correct index of the correct answer
+ * time does get subtracted if the answer is wrong by using the current question number multiplied by a factor of 3
+ */
 function captureAnswer() {
     var choiceSelection = "";
     var userChoice;
@@ -169,12 +224,25 @@ function captureAnswer() {
     }
 }
 
+/**
+ * Generates the next question
+ * Retrieves the current question
+ * Then iterates those the choice list and displays it
+ */
 function nextQuestion() {
-    console.log("NEXT QUESTION!!");
     getQuestion();
     iterateChoices(quizQuestions[currentQuestion].choices);
 }
 
+/**
+ * Saves the score
+ * Gets the user inputted value
+ * pushes the value to the leaderboard array
+ * renames button variable to represent start over
+ * calls nextQuestion() to generate the next page by using the leaderboard objects
+ * THERE IS A BUG - because of how I store the leaderboard using an array and just outputting it
+ * the array output object is kept and there is a , present in the names
+ */
 function saveScore() {
     let initials = document.getElementById("userInitials").value;
     let initialsAndScore = initials + " - " + userCorrectAnswers + "<br>";
@@ -186,18 +254,31 @@ function saveScore() {
     document.getElementById("quizNext").id = "quizStart";
 }
 
+/**
+ * Future function, maybe...
+ */
 function viewLeaderboard() {
     console.log("view leaderboard");
 }
 
+/**
+ * Reset quiz global variables;
+ * init current question back to -1
+ * reset correct answers and sets the time back to original
+*/
 function restartQuiz() {
     currentQuestion = -1;
     userCorrectAnswers = 0;
     quizTime = timelimit;
 }
 
+/**
+ * Clears the leaderboard
+ * Sets the leaderboard to empty, regenerate the webpage by calling the nextQuestion()
+ * to display the leadboard object
+ * Reset the leaderboard back to leaderboard
+*/
 function clearLeaderboard() {
-    console.log("clear");
     leaderboard = [];
     quizQuestions[currentQuestion].choices[0] = "";
     nextQuestion();
